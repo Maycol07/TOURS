65065bcca858e6743ffa3af2946f429a95a3cc54
MAYCOLRICHARDCLAUDIOPINEDO18 <1353921@senati.pe>
2022-11-03 18:55:40 -0500

    <-<-<-<-

---
+++ resources/views/hola.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>MAIK</title>
+		<meta charset="utf-8">
+       <div class="progress">
+          
+       </div>
+            
+        </div>
+		
+	</head>
+	<body>
+		<table border="1">
+            <P>
+                <style>
+                    h1 {
+                    color: blue;
+                    }
+                    h2 {
+                        color: green;
+                    }
+                    h3 {
+                        color: yellow;
+                    }
+                </style>
+			<h1>SENATI</h1>
+            </P>
+			<tr>
+				<th colspan="3">1</th>
+				<th>2</th>
+				<th colspan="3">3
+			</tr>
+			<tr>
+				<td>S</td>
+				<td>E</td>
+				<td>N</td>
+				<td>A</td>
+				<td>T</td>
+                <td>I</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>0</td>
+				<td>2</td>
+				<td colspan="2">2.</td>
+			</tr>
+		</table>
+
+<body>
+    <p>
+    <img src="https://th.bing.com/th/id/OIP.NSzr9WEgAGD8YC4TBHEXiwHaEK?w=321&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+    <p>
+</body>
+
+<h2>HTML, siglas en inglés de HyperText Markup Language (‘lenguaje de marcado de hipertexto’), hace referencia al lenguaje de marcado para la elaboración de páginas web. Es un estándar que sirve de referencia del software que conecta con la elaboración de páginas web en sus diferentes versiones</h2>
+
+
+<body>
+    <p>
+    <img src="https://th.bing.com/th/id/OIP.V_P6klCdkSY1VBxiq0UzuwHaEK?w=301&h=180&c=7&r=0&o=5&pid=1.7" alt="">
+    <p>
+</body>
+
+<h3>
+PHP es un lenguaje de programación de uso general que se adapta especialmente al desarrollo web. Fue creado inicialmente por el programador danés-canadiense Rasmus Lerdorf en 1994. En la actualidad, la implementación de referencia de PHP</h3>
+        
+	</body>
+</html>
